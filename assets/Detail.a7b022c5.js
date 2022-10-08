f97ba35890e0878533e558900505bb81abadc425
xiaocheng555 <1052745517@qq.com>
2022-10-09 03:13:21 +0800

    feat: some

---
+++ assets/Detail.a7b022c5.js
@@ -0,0 +1,1 @@
+import"./Detail.vue_vue_type_script_setup_true_lang.bffa5adf.js";import{_ as t}from"./Detail.vue_vue_type_script_setup_true_lang.bffa5adf.js";import"./index.68c54564.js";export{t as default};
--- script/deploy.js
+++
@@ -1,8 +0,0 @@
-import ghpages from 'gh-pages'
-
-console.log('ghpages.publish...')
-ghpages.publish('dist', {
-  message: 'auto publish'
-}, function (err) { 
-  console.error(err)
-})
