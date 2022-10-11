197044be7e54a4cfa4b62233ca1ab5aa2da44738
xiaocheng555 <1052745517@qq.com>
2022-10-11 14:28:02 +0800

    fix: 解决gh-page报错

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,23 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     },
     extensions: ['.vue', '.js', '.json', '.tsx', '.ts']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // 重写sanitizeFileName方法，不生成‘_’前缀文件，解决vite打包之后生成 ‘_plugin-vue_export-helper.cdc0426e.js’ 文件，gh-page不解析_前缀文件，导致gh-page页面访问该文件报404
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName (name) {
+          const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+          const DRIVE_LETTER_REGEX = /^[a-z]:/i
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return driveLetter + name.substr(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+        }
+      }
+    }
   }
 })
